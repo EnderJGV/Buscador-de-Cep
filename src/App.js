4c1aa9e5dbb23bd538e96cabeccda66f04fc5c96
Ender Josue <ender.guaina@gmail.com>
2022-07-10 09:09:21 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { FiSearch } from 'react-icons/fi'
+import './style.css';
+import api from './services/api';
 
 function App() {
+
+  const[input,setInput] = useState('')
+  const[cep,setCep] = useState({})
+
+  async function handleSearch(){
+    //01001000/json/
+
+    if(input === ""){
+      alert("Precha algum CEP");
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    }catch{
+      alert("Ops erro ao buscar aqui!");
+      setInput("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <h1 className="title">Buscador Cep</h1>
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep..."
+        value={input}
+        onChange={(e)=>{
+          setInput(e.target.value)
+        }}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+        <FiSearch size={25} color="#fff"/>
+      </button>
+      </div>
+      
+        {Object.keys(cep).length > 0 && (
+          <main className='main'>
+            <h2>{cep.cep}</h2>
+        
+            <span>CEP: {cep.logradouro}</span>
+            <span>complemento:{cep.complemento}</span>
+            <span>{cep.bairro}</span>
+            <span>{cep.localidade} - {cep.uf}</span>
+            <span>{cep.ibge}</span>
+            <span>{cep.gia}</span>
+            <span>{cep.ddd}</span>
+            <span>{cep.siafi}</span>
+          </main>
+        )}
+
+
+
     </div>
   );
 }
